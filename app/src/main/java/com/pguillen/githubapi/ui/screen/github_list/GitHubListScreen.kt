c872eafbb03f3a04638b251da94409d5e09b169e
Pedro Guillén <pedro.guillen55@gmail.com>
2026-01-08 10:22:13 +0100

    Added a lot of changes

--- app/src/main/java/com/pguillen/githubapi/ui/screen/github_list/GitHubListScreen.kt
+++ app/src/main/java/com/pguillen/githubapi/ui/screen/github_list/GitHubListScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.material3.ListItem
 import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
--- app/src/test/java/com/pguillen/githubapi/viewmodels/GitHubListViewModelTest.kt
+++ app/src/test/java/com/pguillen/githubapi/viewmodels/GitHubListViewModelTest.kt
@@ -11,7 +11,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
@@ -23,85 +22,85 @@ import org.junit.Test
 
 class GitHubListViewModelTest {
 
-    private val testDispatcher = StandardTestDispatcher()
-    private lateinit var fakeGetUserRepos: FakeGetUserRepos
-    private lateinit var viewModel: GitHubListViewModel
+	private val testDispatcher = StandardTestDispatcher()
+	private lateinit var fakeGetUserRepos: FakeGetUserRepos
+	private lateinit var viewModel: GitHubListViewModel
 
-    @OptIn(ExperimentalCoroutinesApi::class)
-    @Before
-    fun setup() {
-        Dispatchers.setMain(testDispatcher)
-        fakeGetUserRepos = FakeGetUserRepos()
-        viewModel = GitHubListViewModel(fakeGetUserRepos)
-    }
+	@OptIn(ExperimentalCoroutinesApi::class)
+	@Before
+	fun setup() {
+		Dispatchers.setMain(testDispatcher)
+		fakeGetUserRepos = FakeGetUserRepos()
+		viewModel = GitHubListViewModel(fakeGetUserRepos)
+	}
 
-    @OptIn(ExperimentalCoroutinesApi::class)
-    @After
-    fun tearDown() {
-        Dispatchers.resetMain()
-    }
+	@OptIn(ExperimentalCoroutinesApi::class)
+	@After
+	fun tearDown() {
+		Dispatchers.resetMain()
+	}
 
-    @Test
-    fun check_initial_values_of_viewmodel() {
-        assertEquals(GitHubListUiState.EmptyList, viewModel.uiState.value)
-        assertTrue(viewModel.currentText.value == "")
-    }
+	@Test
+	fun check_initial_values_of_viewmodel() {
+		assertEquals(GitHubListUiState.EmptyList, viewModel.uiState.value)
+		assertTrue(viewModel.currentText.value == "")
+	}
 
-    @Test
-    fun transition_from_empty_list_to_loading_when_searching() = runTest {
-	    viewModel.onEvent(GitHubListUiEvent.OnTextChange("Text"))
-        viewModel.onEvent(GitHubListUiEvent.OnSearchClick)
-        assertEquals(GitHubListUiState.Loading, viewModel.uiState.value)
-    }
+	@Test
+	fun transition_from_empty_list_to_loading_when_searching() = runTest {
+		viewModel.onEvent(GitHubListUiEvent.OnTextChange("Text"))
+		viewModel.onEvent(GitHubListUiEvent.OnSearchClick)
+		assertEquals(GitHubListUiState.Loading, viewModel.uiState.value)
+	}
 
-    @Test
-    fun transition_from_loading_to_success_when_repository_returns_data() = runTest {
-        fakeGetUserRepos.repos = listOf(createRepoDomain(1))
-        val reposUi = fakeGetUserRepos.repos.map { it.toUi() }
-        viewModel.onEvent(GitHubListUiEvent.OnTextChange("Text"))
-        viewModel.onEvent(GitHubListUiEvent.OnSearchClick)
-        testDispatcher.scheduler.advanceUntilIdle()
-        assertEquals(GitHubListUiState.Success(reposUi), viewModel.uiState.value)
-    }
+	@Test
+	fun transition_from_loading_to_success_when_repository_returns_data() = runTest {
+		fakeGetUserRepos.repos = listOf(createRepoDomain(1))
+		val reposUi = fakeGetUserRepos.repos.map { it.toUi() }
+		viewModel.onEvent(GitHubListUiEvent.OnTextChange("Text"))
+		viewModel.onEvent(GitHubListUiEvent.OnSearchClick)
+		testDispatcher.scheduler.advanceUntilIdle()
+		assertEquals(GitHubListUiState.Success(reposUi), viewModel.uiState.value)
+	}
 
-    @Test
-    fun transition_from_loading_to_empty_list_when_repository_does_not_return_data() = runTest {
-	    viewModel.onEvent(GitHubListUiEvent.OnTextChange("Text"))
-        viewModel.onEvent(GitHubListUiEvent.OnSearchClick)
-        assertEquals(GitHubListUiState.Loading, viewModel.uiState.value)
-        testDispatcher.scheduler.advanceUntilIdle()
-        assertEquals(GitHubListUiState.EmptyList, viewModel.uiState.value)
-    }
+	@Test
+	fun transition_from_loading_to_empty_list_when_repository_does_not_return_data() = runTest {
+		viewModel.onEvent(GitHubListUiEvent.OnTextChange("Text"))
+		viewModel.onEvent(GitHubListUiEvent.OnSearchClick)
+		assertEquals(GitHubListUiState.Loading, viewModel.uiState.value)
+		testDispatcher.scheduler.advanceUntilIdle()
+		assertEquals(GitHubListUiState.EmptyList, viewModel.uiState.value)
+	}
 
-    @Test
-    fun transition_from_loading_to_error_when_repository_returns_an_error() = runTest {
-        fakeGetUserRepos.shouldThrowError = true
-	    viewModel.onEvent(GitHubListUiEvent.OnTextChange("Text"))
-        viewModel.onEvent(GitHubListUiEvent.OnSearchClick)
-        assertEquals(GitHubListUiState.Loading, viewModel.uiState.value)
-        testDispatcher.scheduler.advanceUntilIdle()
-        assertEquals(GitHubListUiState.Error("Error cargando repos."), viewModel.uiState.value)
-    }
+	@Test
+	fun transition_from_loading_to_error_when_repository_returns_an_error() = runTest {
+		fakeGetUserRepos.shouldThrowError = true
+		viewModel.onEvent(GitHubListUiEvent.OnTextChange("Text"))
+		viewModel.onEvent(GitHubListUiEvent.OnSearchClick)
+		assertEquals(GitHubListUiState.Loading, viewModel.uiState.value)
+		testDispatcher.scheduler.advanceUntilIdle()
+		assertEquals(GitHubListUiState.Error("Error cargando repos."), viewModel.uiState.value)
+	}
 
-    @Test
-    fun change_text_when_typing_in_text_field() {
-        viewModel.onEvent(GitHubListUiEvent.OnTextChange("Test"))
-        assertEquals("Test", viewModel.currentText.value)
-    }
+	@Test
+	fun change_text_when_typing_in_text_field() {
+		viewModel.onEvent(GitHubListUiEvent.OnTextChange("Test"))
+		assertEquals("Test", viewModel.currentText.value)
+	}
 
-    @Test
-    fun emits_error_effect_when_search_text_is_blank() = runTest {
-        viewModel.onEvent(GitHubListUiEvent.OnSearchClick)
-        val effect = viewModel.uiEffect.first()
-        assertEquals(GitHubListUiEffect.ShowSnackbar("Introduce un texto"), effect)
-    }
+	@Test
+	fun emits_error_effect_when_search_text_is_blank() = runTest {
+		viewModel.onEvent(GitHubListUiEvent.OnSearchClick)
+		val effect = viewModel.uiEffect.first()
+		assertEquals(GitHubListUiEffect.ShowSnackbar("Introduce un texto"), effect)
+	}
 
-    @Test
-    fun emits_error_effect_when_repository_returns_an_error() = runTest {
-        fakeGetUserRepos.shouldThrowError = true
-	    viewModel.onEvent(GitHubListUiEvent.OnTextChange("Text"))
-        viewModel.onEvent(GitHubListUiEvent.OnSearchClick)
-	    val effect = viewModel.uiEffect.first()
-        assertEquals(GitHubListUiEffect.ShowSnackbar("No se ha podido cargar los repos"), effect)
-    }
+	@Test
+	fun emits_error_effect_when_repository_returns_an_error() = runTest {
+		fakeGetUserRepos.shouldThrowError = true
+		viewModel.onEvent(GitHubListUiEvent.OnTextChange("Text"))
+		viewModel.onEvent(GitHubListUiEvent.OnSearchClick)
+		val effect = viewModel.uiEffect.first()
+		assertEquals(GitHubListUiEffect.ShowSnackbar("No se ha podido cargar los repos"), effect)
+	}
 }
